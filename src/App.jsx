7f9df7cf1224f4da328b73b484348dd9a20b29d6
veeraj-k <veerajkuppila2004@gmail.com>
2025-05-31 19:18:55 +0530

    auto logout - axios intercepter

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import axios from "axios";
+import axiosInstance from "./utils/axiosConfig";
+
+// Set axios as the default instance
+axios.defaults = axiosInstance.defaults;
+
 function App() {
   return (
     <>
