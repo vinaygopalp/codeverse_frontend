23e33fa7be13d270fc6e08ca0477315ee553b5f3
vinaygopal <150654839+vinaygopalp@users.noreply.github.com>
2025-06-02 11:39:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ['codeverse-nw1r.onrender.com'], 
+  },
 });
